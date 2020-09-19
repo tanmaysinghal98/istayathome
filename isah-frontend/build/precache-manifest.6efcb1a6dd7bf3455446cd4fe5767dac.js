626ef527b5b7abe150b2d402c35a75972cb0da35
Tanmay Singhal <tanmaysinghal98@Tanmays-MacBook-Pro.local>
2020-09-20 01:39:19 +0530

    add ScreenShot into media branch

---
+++ isah-frontend/build/precache-manifest.6efcb1a6dd7bf3455446cd4fe5767dac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd90bf97285d89e0d5dfd83242bd0e0",
+    "url": "https://istayathome.in/index.html"
+  },
+  {
+    "revision": "617d705ca58673875028",
+    "url": "https://istayathome.in/static/css/main.5d10b3ff.chunk.css"
+  },
+  {
+    "revision": "212bbb8385356a74fc38",
+    "url": "https://istayathome.in/static/js/2.38e98d1e.chunk.js"
+  },
+  {
+    "revision": "b15c5dd43324fe0656e95bfa6596d438",
+    "url": "https://istayathome.in/static/js/2.38e98d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617d705ca58673875028",
+    "url": "https://istayathome.in/static/js/main.e1b17891.chunk.js"
+  },
+  {
+    "revision": "cd20b22ee18548c36a61",
+    "url": "https://istayathome.in/static/js/runtime-main.6e6d9276.js"
+  },
+  {
+    "revision": "b98ce243a18198056e658cf9c6639e9d",
+    "url": "https://istayathome.in/static/media/full_colored_logo.b98ce243.png"
+  },
+  {
+    "revision": "aceed990eefbadc6ef02236543cc6837",
+    "url": "https://istayathome.in/static/media/full_monochrome_logo_transparent.aceed990.png"
+  }
+]);
